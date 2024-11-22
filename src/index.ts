f2ecf2ad7cf2fdeadc2275cf04b050847a402b3b
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-11-22 23:03:39 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,11 @@ const array = [1, 3, 5, 7, 8, 9, 10, 12, 13, 15]
 const cnf = NormalForm.mnf(4, array)
 const dnf = NormalForm.mnf(4, NormalForm.exclude(4, array))
 
-console.log(LatexNormalForm.tableNf("dis", dnf))
-console.log(LatexNormalForm.tableNf("con", cnf))
+console.log(LatexNormalForm.nf("dis", 4, dnf.pnf))
+console.log(LatexNormalForm.selections("dis", dnf.selections))
+console.log(LatexNormalForm.tableNf("dis", dnf, { showUsedImplicants: true }))
+console.log(LatexNormalForm.nf("dis", 4, dnf.result))
+console.log(LatexNormalForm.nf("con", 4, cnf.pnf))
+console.log(LatexNormalForm.selections("con", cnf.selections))
+console.log(LatexNormalForm.tableNf("con", cnf, { showUsedImplicants: true }))
+console.log(LatexNormalForm.nf("con", 4, cnf.result))
